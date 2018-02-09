855646e2e44245284d447ce77da350e02577ff43
Filip Loster <filip.loster@pragmaticcoders.com>
2018-02-09 16:47:48 +0100

    added add operation

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,17 +1,127 @@
 import * as R from 'ramda';
 
+interface ParsedToken {
+  op: string;
+  num: number;
+}
+
+interface Result {
+  success: boolean;
+  stack: number[];
+}
+
+type OpResult = [ParsedToken, Result];
+type OpImplementation = (data: number[]) => number;
+type Operation = (tokenAndResult: OpResult) => OpResult;
+
 export function calculate(input: string): number {
-  return R.reduce<number, number>(R.add, 0, prepareInput(input));
+  return R.pipe(
+    prepareInput,
+    R.reduce<ParsedToken, number[]>(parseStack, []),
+    getResultFromCalculationStack
+  )(input);
 }
 
-function prepareInput(input: string): number[] {
-  return R.map(toNumber, tokenize(input));
+function parseStack(stack: number[], token: ParsedToken): number[] {
+  if (isNumber(token)) {
+    return stack.concat(token.num);
+  } else {
+    return performOperationOnStack(stack, token);
+  }
 }
 
-function toNumber(value: string): number {
-  return Number(value);
+function performOperationOnStack(
+  stack: number[],
+  token: ParsedToken
+): number[] {
+  let result: Result = {
+    success: false,
+    stack: stack
+  };
+
+  return R.pipe(defineOperation('+', 2, addOperation), invalidOperation())([
+    token,
+    result
+  ])[1].stack;
+}
+
+function defineOperation(
+  opSymbol: String,
+  numberOfArguments: number,
+  opImplementation: OpImplementation
+): Operation {
+  return (tokenAndResult: OpResult): OpResult => {
+    let token = tokenAndResult[0];
+    let result = tokenAndResult[1];
+
+    if (result.success) {
+      return tokenAndResult;
+    }
+    if (opSymbol != null && token.op != opSymbol) {
+      return tokenAndResult;
+    }
+    if (result.stack.length < numberOfArguments) {
+      throw new SyntaxError('Insufficient number of arguments');
+    }
+
+    const slicedStack = R.splitAt<number>(
+      result.stack.length - numberOfArguments,
+      result.stack
+    );
+    const remainingStack = slicedStack[0];
+    const opArguments = slicedStack[1];
+    const operationResult = opImplementation(opArguments);
+    const resultStack = R.concat<number>(remainingStack, [operationResult]);
+
+    return [token, { success: true, stack: resultStack }];
+  };
+}
+
+function invalidOperation(): Operation {
+  return (tokenAndResult: OpResult): OpResult => {
+    let token = tokenAndResult[0];
+    let result = tokenAndResult[1];
+
+    if (result.success) return tokenAndResult;
+
+    throw new SyntaxError('Invalid operation: ' + token.op);
+  };
+}
+
+function addOperation(data: number[]): number {
+  return R.sum(data);
+}
+
+function getResultFromCalculationStack(stack: number[]): number {
+  if (stack.length != 1) {
+    throw new SyntaxError('Insufficient number of operations');
+  }
+
+  return R.sum(stack);
+}
+
+function prepareInput(input: string): ParsedToken[] {
+  return R.map(parseToken, tokenize(input));
+}
+
+function parseToken(token: string): ParsedToken {
+  return {
+    num: Number(token),
+    op: token
+  };
 }
 
 function tokenize(input: string): string[] {
-  return input.split(' ');
+  return input
+    .replace(/\s+/g, ' ')
+    .trim()
+    .split(' ');
+}
+
+function isNumber(token: ParsedToken): boolean {
+  return !isOperation(token);
+}
+
+function isOperation(token: ParsedToken): boolean {
+  return Number.isNaN(token.num);
 }
--- test/calculator.spec.ts
+++ test/calculator.spec.ts
@@ -2,11 +2,51 @@ import { assert, expect } from 'chai';
 import { calculate } from '../src/calculator';
 
 context('#calculate', () => {
-  it('should return single number', () => {
-    expect(calculate('1')).to.eq(1);
+  describe('Input parsing', () => {
+    it('should return 0 for empty data', () => {
+      expect(calculate('')).to.eq(0);
+    });
+
+    it('should return single number', () => {
+      expect(calculate('1')).to.eq(1);
+    });
+
+    it('should return single double digit number', () => {
+      expect(calculate('22')).to.eq(22);
+    });
+
+    it('should throw when not a number entered', () => {
+      expect(() => {
+        calculate('test');
+      }).to.throw('Invalid operation: test');
+    });
+
+    it('should throw when two numbers without operation are given', () => {
+      expect(() => {
+        calculate('1 1');
+      }).to.throw('Insufficient number of operations');
+    });
+
+    it('should ignore excessive spaces', () => {
+      expect(calculate('    1     ')).to.eq(1);
+    });
   });
 
-  it('should return single double digit number', () => {
-    expect(calculate('22')).to.eq(22);
+  describe('Addition', () => {
+    it('should accept "+" as a valid parameter', () => {
+      calculate('1 1 +');
+    });
+
+    it('should throw when not enough operations given', () => {
+      expect(() => {
+        calculate('1 +');
+      }).to.throw('Insufficient number of arguments');
+    });
+
+    it('should throw when too many parameters given', () => {
+      expect(() => {
+        calculate('1 1 1 +');
+      }).to.throw('Insufficient number of operations');
+    });
   });
 });
