d6321090fa6b1c00826892b0f2897d0a2792ee76
Filip Loster <filip.loster@pragmaticcoders.com>
2018-02-09 13:58:10 +0100

    implemented argument parsing

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,7 +1,17 @@
+import * as R from 'ramda';
 
+export function calculate(input: string): number {
+  return R.reduce<number, number>(R.add, 0, prepareInput(input));
+}
 
-export function calculate(
-    input : string
-): number {
-    return 1;
+function prepareInput(input: string): number[] {
+  return R.map(toNumber, tokenize(input));
+}
+
+function toNumber(value: string): number {
+  return Number(value);
+}
+
+function tokenize(input: string): string[] {
+  return input.split(' ');
 }
--- test/calculator.spec.ts
+++ test/calculator.spec.ts
@@ -1,13 +1,12 @@
 import { assert, expect } from 'chai';
-import {calculate} from '../src/calculator';
+import { calculate } from '../src/calculator';
 
 context('#calculate', () => {
-   it('should return single number', () =>{
-      expect(calculate("1")).to.eq(1);
-   });
+  it('should return single number', () => {
+    expect(calculate('1')).to.eq(1);
+  });
 
-   it('should return single double digit number', () => {
-      expect(calculate('22')).to.eq(22);
-   });
-
+  it('should return single double digit number', () => {
+    expect(calculate('22')).to.eq(22);
+  });
 });
